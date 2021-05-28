e170d31cdea6b60128f31023bb725f7108d7858d
guoyu <1521701523@qq.com>
2021-05-28 18:05:45 +0800

    update

--- web/src/test/java/BaseTest.java
+++ web/src/test/java/BaseTest.java
@@ -1,9 +1,11 @@
+import com.alibaba.fastjson.JSONObject;
 import com.anshark.GyadminApplication;
 import com.anshark.dao.GyMenusDao;
 import com.anshark.model.GyMenus;
 import com.anshark.utils.SpringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,10 +21,20 @@ import java.util.List;
 @SpringBootTest(classes = GyadminApplication.class)
 public class BaseTest {
 
+    @Autowired
+    private GyMenusDao gyMenusDao;
+
     @Test
     public void test1(){
         GyMenusDao bean = SpringUtils.getBean(GyMenusDao.class);
         List<GyMenus> menusBy = bean.getMenusBy(0, 0, Arrays.asList(1, 2));
         System.out.println(menusBy);
+    }
+
+    @Test
+    public void test2(){
+        List<GyMenus> menuInfo = gyMenusDao.getMenusBy(4, 2, Arrays.asList(5,6,7));
+
+        System.out.println(JSONObject.toJSONString(menuInfo));
     }
 }
